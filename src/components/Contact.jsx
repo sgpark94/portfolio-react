3ad6a746fcbeee0c1941e16843c4a5523cb66ed5
sgpark94 <olloyy@naver.com>
2024-01-09 13:24:35 +0900

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,54 @@
 import React from "react";
 // 리액트를 사용하기 위해 리액트 라이브러리를 불러오고 있다. 이렇게 하면 리액트의 기능과 컴포넌트를 사용할 수 있다.
 
+const contactText = [
+  {
+    link: "https://open.kakao.com/o/s07MGH1f",
+    title: "KAKAO : 박슬기",
+  },
+  {
+    link: "mailto:peach_crush@kakao.com",
+    title: "MAIL : peach_crush@kakao.com",
+  },
+];
+
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <section id="contact">
+      <div className="contact__inner">
+        <h2 className="contact__title">Contact</h2>
+        <div className="contact__lines" aria-hidden="true">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </div>
+        <div className="contact__text">
+          <div className="text">
+            {contactText.map((contact, key) => (
+              <div key={key}>
+                <a href={contact.link} target="_blank" rel="noreferrer">
+                  {contact.title}
+                </a>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="contact__lines bottom" aria-hidden="true">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
